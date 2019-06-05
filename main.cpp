e90413700ecdb0f16a283805b14877b704cb10f7
Kevinlinpr <kevinlinpr@gmail.com>
2019-06-05 23:52:46 +0800

    循环单链表完成头插法

--- main.cpp
+++ main.cpp
@@ -6,12 +6,10 @@ int main(){
     int a[3]={12,29,30};
     PracticeLinearList<int> practiceLinearListA(a,3);
     std::cout<<"In heap"<<std::endl;
-    auto * dLinkListInHeap = new DLinkList<int>;
-    dLinkListInHeap->EndInsertCreateList(practiceLinearListA);
-    dLinkListInHeap->PrintList();
-    DNode<int> newNode(99);
-    dLinkListInHeap->BeforeInsert(1,newNode);
-    dLinkListInHeap->PrintList();
-    dLinkListInHeap->DeleteElem(2);
-    dLinkListInHeap->PrintList();
+    auto * lLinkListInHeap = new LLinkList<int>;
+    lLinkListInHeap->HeadInsertCreateList(practiceLinearListA);
+    lLinkListInHeap->PrintList();
+    std::cout<<lLinkListInHeap->GetElem(3)->next->next->
+    next->next->next->next->next->next->next->next->
+    next->next->next->next->next->next->next->next->data<<std::endl;
 }
